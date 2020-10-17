2c0c231ee08b28a3a43b901bad601b3c17defd47
Josh Bressers <josh@bress.net>
2020-10-17 14:28:54 -0500

    Update the tests

--- test_chain.py
+++ test_chain.py
@@ -1,13 +1,13 @@
 #!/bin/env python
 
 import unittest
-import dwf_chain
+import vuln_chain
 
 
 class TestBlock(unittest.TestCase):
 
     def test_create(self):
-        the_block = dwf_chain.Block('id', 0, 'prev_hash', 'hash')
+        the_block = vuln_chain.Block('id', 0, 'prev_hash', 'hash')
 
         self.assertEqual(the_block.get_date(), 0, "Should be 0")
         self.assertEqual(the_block.get_id(), 'id', "Should be 'id'")
@@ -18,7 +18,7 @@ class TestBlock(unittest.TestCase):
 
         hash = "ec81332e528ff522bfb05e93cd826c075ec26c5b9cc5bfcf7087d6f28da90e1d4f44df2a9af32573df5ab535d4b985731001c71d9f7629170518bba95280ef3d"
 
-        the_block = dwf_chain.Block('id', 0, 'prev_hash')
+        the_block = vuln_chain.Block('id', 0, 'prev_hash')
         output = the_block.gen_hash()
 
         self.assertEqual(output, hash, "Should be '%s'" % hash)
@@ -27,11 +27,11 @@ class TestBlock(unittest.TestCase):
 
         hash = "ec81332e528ff522bfb05e93cd826c075ec26c5b9cc5bfcf7087d6f28da90e1d4f44df2a9af32573df5ab535d4b985731001c71d9f7629170518bba95280ef3d"
 
-        the_block = dwf_chain.Block('id', 0, 'prev_hash', hash)
+        the_block = vuln_chain.Block('id', 0, 'prev_hash', hash)
 
         self.assertTrue(the_block.verify())
 
-        the_block = dwf_chain.Block('id', 0, 'prev_hash', 'bad_hash')
+        the_block = vuln_chain.Block('id', 0, 'prev_hash', 'bad_hash')
         self.assertFalse(the_block.verify())
 
 if __name__ == '__main__':
