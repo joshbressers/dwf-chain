d360a67135ea4980f01fa42d500f846443f0b162
Josh Bressers <josh@bress.net>
2020-10-18 20:38:54 -0500

    Move the module and tests around

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vuln-chain-joshbressers", # Replace with your own username
+    version="0.0.1",
+    author="Josh Bressers",
+    author_email="josh@bress.net",
+    description="A library for storing vulnerability info in a blockchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joshbressers/dwf-chain",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
--- tests/test_chain.py
+++ tests/test_chain.py
@@ -1,5 +1,9 @@
 #!/bin/env python
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 
 import vuln_chain
